10c347b57bec2f827e9cc9238c1f3d1a8714e1b9
Christina <develop@pirron.one>
2019-12-11 21:16:00 +0100

    added the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import PictureCard from "./components/PictureCard";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <h1>NASA</h1>
         <h3>Image of the Day</h3>
+        <PictureCard/>
       </div>
     </div> 
   );
---
+++ src/components/PictureCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Picture from "./Picture";
+
+function PictureCard() {
+    return <div>
+                This is PictureCard.js
+                <Picture />
+            </div>
+    
+}
+
+export default PictureCard
