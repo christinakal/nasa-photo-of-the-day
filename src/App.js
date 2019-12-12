32b0b7cb8fbedf07ee30ba89632b385185868145
Christina <develop@pirron.one>
2019-12-12 21:30:52 +0100

    added navbar and card components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Picture from "./components/Picture";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 function App() {
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
     <div className="App">
-      <div>
-        <h1>NASA</h1>
-        <h3>Image of the Day</h3>
+      <div>  
+          <Navbar color="faded" light>
+            <NavbarBrand href="/" className="mr-auto">NASA</NavbarBrand>
+            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="https://www.nasa.gov/">About</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">Contact</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
         <Picture />
       </div>
     </div> 
@@ -15,3 +33,35 @@ function App() {
 }
 
 export default App;
+
+
+
+{/* <Navbar color="faded" light>
+  <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+  <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+  <Collapse isOpen={!collapsed} navbar>
+    <Nav navbar>
+      <NavItem>
+        <NavLink href="/components/">Components</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar> */}
+
+
+{/* <Navbar style={{ fontFamily:'Montserrat', }}>
+          <NavbarBrand href="/" className="mr-auto" style={{color:'#464159'}}><h1>NASA</h1> Image of the Day</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar />
+            <NavItem style={{listStyleType:'none'}}>
+              <NavLink href="https://www.nasa.gov/" target="_blank" style={{textDecoration: 'none'}}>About</NavLink>
+            </NavItem>
+            <NavItem style={{listStyleType:'none'}}>
+              <NavLink href="https://github.com/reactstrap/reactstrap" style={{textDecoration: 'none'}}>Contact</NavLink>
+            </NavItem>
+          </Collapse>
+        </Navbar> */}
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Modal, ModalHeader, ModalBody, ModalFooter
+  } from 'reactstrap';
 
 const PictureCard = props => {
-    console.log("Props:", props)
+
+const [modal, setModal] = useState(false);
+
+const toggle = () => setModal(!modal);
+
     return <div key={props.index}>
-                <h3>{props.title}</h3>
-                <img als={props.title} src={props.url}/>
-                <div>Photo taken at: {props.date}</div>
-                <h3>Description</h3>
-                <p>{props.explanation}</p>
+                <Card style={{maxWidth:'1000px', margin:'0 auto'}}>
+                    <CardImg top width="100%" src={props.url} alt="Card image cap" />
+                    <CardBody>
+                        <CardSubtitle>{props.title}</CardSubtitle>
+                        <CardText></CardText>
+                        <Button>Description</Button>
+                    </CardBody>
+                </Card>
             </div>
     
 }
 
 export default PictureCard; 
+
+
+{/* <Card>
+    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+    <CardBody>
+        <CardTitle>Card title</CardTitle>
+        <CardSubtitle>Card subtitle</CardSubtitle>
+        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+        <Button>Button</Button>
+    </CardBody>
+</Card> */}
+
+
+// <h3>Image of the day: {props.title}</h3>
+// <img als={props.title} src={props.url}/>
+// <div>Photo taken at: {props.date}</div>
+// <h3>Description</h3>
+// <p>{props.explanation}</p>
